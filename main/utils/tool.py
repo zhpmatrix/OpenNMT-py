56ce593e5e24edc7f1d1eb190ae8dc1588f06fbb
Ubuntu <eigen@ng2-test-southeast-asia.104seguci44e1kt2gy0pmkstih.ix.internal.cloudapp.net>
2020-01-10 14:54:26 +0000

    update tool for tensorboard

--- main/utils/tool.py
+++ main/utils/tool.py
@@ -1,12 +1,14 @@
 from tensorboard.backend.event_processing import event_accumulator
-root_dir = '../runs/4/'
+root_dir = '/data/zhanghaipeng/seq2seq/s2s/logs/'
+file_name = 'Jan-10_14-51-08/'
 log_path = 'events.out.tfevents.1573803254.hpc4.ipa.aidigger.com'
-ea=event_accumulator.EventAccumulator(root_dir+log_path)
+ea=event_accumulator.EventAccumulator(root_dir+file_name+log_path)
 ea.Reload()
 print(ea.scalars.Keys())
 
-det_acc=ea.scalars.Items('eval_detection_accuracy')
-corr_acc=ea.scalars.Items('eval_correction_accuracy')
-for det, corr in zip(det_acc,corr_acc):
-    if det.step >= 8000:
-        print(det.step, det.value, corr.value)
+xents=ea.scalars.Items('valid/xent')
+ppls=ea.scalars.Items('valid/ppl')
+accs=ea.scalars.Items('valid/accuracy')
+for xent, ppl, acc in zip(xents,ppls,accs):
+    if xent.step >= 10000:
+        print(xent.step, xnet.value, ppl.value, acc.value)
